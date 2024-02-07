39496f6e90a2181c37c311a939cf285009bcba15
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 13:58:24 -0500

    Create Code
    We add students to a class roster until there are no more students. Then print out the count of the students to the Console.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			List<string> studentnames = new List<string>();
+			int studentcount = 0;
+			string morestudents;
+
+			do
+			{
+				Console.Write("Enter Student Name: ");
+				studentnames.Add(Console.ReadLine());
+				studentcount++;
+				Console.Write("More Students (yes/no): ");
+				morestudents = Console.ReadLine();
+			} while ( morestudents == "yes" );
+
+			Console.WriteLine($"You entered {studentcount} students.");
+
+			_ = Console.ReadLine();
 		}
 	}
 }
